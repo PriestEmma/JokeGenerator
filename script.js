ce76b997a2575e7689b67304ab3349858c20c72c
PriestEmma <essienemmanuel243@gmail.com>
2026-07-21 08:03:22 +0100

    updated files

--- script.js
+++ script.js
@@ -1,17 +1,24 @@
 // Using XmlhttpRequest
 const jokeBtn = document.querySelector('#joke-btn');
 const div = document.getElementById('joke');
+const response = document.querySelector('.res');
 
 function generateJoke() {
   const xhr = new XMLHttpRequest();
 
-  xhr.open('GET', 'https://api.chucknorris.io/jokes/random');
+  xhr.open('GET', 'https://v2.jokeapi.dev/joke/Programming,Spooky,Christmas');
 
   xhr.onreadystatechange = function () {
     if (this.status === 200 && this.readyState === 4) {
       const joke = JSON.parse(this.responseText);
 
-      div.innerHTML = joke.value;
+      if (joke.joke) {
+        div.innerHTML = joke.joke;
+        response.innerHTML = '';
+      } else {
+        div.innerHTML = joke.setup;
+        response.innerHTML = joke.delivery;
+      }
     } else {
       div.innerHTML = 'Something Went Wrong (Not funny)';
     }
